a1d2f053f97695233fbaafce37ecfcef84eeaaf9
beholdTheSpoon <nokko.nieminen@gmail.com>
2022-04-04 14:19:27 +0200

    fd

--- poblacions.php
+++ poblacions.php
@@ -4,29 +4,10 @@
 	$contrasenya = "";
 	$basededades = "informacio";
 
-	//retornen tot
-	if( isset($_GET['tots']) ){
+	//Retornem nomes la provincia passat per paràmetre
+	if( isset($_GET['provincies']) ){
 		$mysqli = new mysqli($host, $usuariBBDD, $contrasenya, $basededades);
-		$sql = "SELECT codiProvincia, nom, cp FROM poblacions;";
-		
-		if ( ! $result = $mysqli->query($sql) ) {
-			echo "No s'ha pogut realitzar la consulta";
-			echo mysqli_error();
-			exit;
-		}
-		else
-		$dades = array();
-		while ($row = $result->fetch_assoc()){
-			array_push($dades, $row);
-		}
-		$dades = utf8_converter($dades);
-		print json_encode(  $dades  );
-	}	
-	
-	//Retornem nomes la poblacio passat per paràmetre
-	if( isset($_GET['id']) ){
-		$mysqli = new mysqli($host, $usuariBBDD, $contrasenya, $basededades);
-		$sql = "SELECT * FROM poblacions WHERE nom = " . $_GET['nom'];
+		$sql = "SELECT * FROM provincies";
 		if ( ! $result = $mysqli->query($sql) ) {
 			echo "No s'ha pogut realitzar la consulta";
 			echo mysqli_error();
@@ -39,6 +20,23 @@
 		$dades = utf8_converter($dades);
 		print json_encode( $dades );
 	}
+	
+	if( isset($_GET['codi']) ){
+		$mysqli = new mysqli($host, $usuariBBDD, $contrasenya, $basededades);
+		$sql = "SELECT * FROM poblacions WHERE codiProvincia = " . $_GET['codi'];
+		if ( ! $result = $mysqli->query($sql) ) {
+			echo "No s'ha pogut realitzar la consulta";
+			echo mysqli_error();
+			exit;
+		}
+		$dades = array();
+		while ($row = $result->fetch_assoc()){
+			array_push($dades, $row);
+		}
+		$dades = utf8_converter($dades);
+		print json_encode( $dades );
+	}
+
 
 	//Conversió recursiva del array a UTF-8
 	function utf8_converter($array)
